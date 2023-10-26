8d521b38beb769395d0d4228aa8e6710a5c95e15
rauf.akchurin <abdurauf.dev@gmail.com>
2023-10-26 22:50:35 +0500

    files for deploy

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import uvicorn
 from fastapi import APIRouter
 from fastapi import FastAPI
 
+import settings
 from api.handlers import user_router
 from api.login_handler import login_router
+from api.service import service_router
 
 # BLOCK WITH ROUTERS
 #############################
@@ -19,7 +21,8 @@ app = FastAPI(title="luchanos-oxford-iniversity")
 main_api_router = APIRouter()
 main_api_router.include_router(user_router, prefix="/user", tags=["user"])
 main_api_router.include_router(login_router, prefix="/login", tags=["login"])
+main_api_router.include_router(service_router, tags=["service"])
 app.include_router(main_api_router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=settings.APP_PORT)
--- settings.py
+++ settings.py
@@ -7,10 +7,13 @@ REAL_DATABASE_URL = env.str(
     default="postgresql+asyncpg://postgres:postgres@0.0.0.0:5434/postgres",
 )
 
+APP_PORT = env.int("APP_PORT", default=8000)
+SECRET_KEY: str = env.str("SECRET_KEY", default="secret_key")
+ALGORITHM: str = env.str("ALGORITHM", default="HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES: int = env.int("ACCESS_TOKEN_EXPIRE_MINUTES", default=30)
+
 TEST_DATABASE_URL = env.str(
     "TEST_DATABASE_URL",
     default="postgresql+asyncpg://postgres_test:postgres_test@0.0.0.0:5433/postgres_test",
 )
-SECRET_KEY: str = env.str("SECRET_KEY", default="secret_key")
-ALGORITHM: str = env.str("ALGORITHM", default="HS256")
-ACCESS_TOKEN_EXPIRE_MINUTES: int = env.int("ACCESS_TOKEN_EXPIRE_MINUTES", default=30)
+
